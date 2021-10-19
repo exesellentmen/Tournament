bd0b62fb86d9696a140b797ddcf90fba67c7fc3c
Pavel <exesellentmen@mail.ru>
2021-10-19 14:37:36 +0300

    Correct Front End

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,26 +3,11 @@
 namespace App\Controller;
 
 use App\Service\Game\GameGenerator\DIContainer\DIContainer;
-use App\Service\Game\GameGenerator\Participant\ParticipantGeneratorInterface;
-use App\Service\Game\GameGenerator\Player\PlayerGenerator;
 use App\Service\Game\GameGenerator\Player\PlayerGeneratorInterface;
-use App\Service\Game\Tournament\InformationInterface;
 use App\Service\Game\Tournament\KnightTournament\Config\ConfigKnightTournament;
 use App\Service\Game\Tournament\KnightTournament\DIContainer\DIContainerTournament;
-use App\Service\Game\Tournament\KnightTournament\Game\SwordFighting\Armor\Shield;
-use App\Service\Game\Tournament\KnightTournament\Game\SwordFighting\Battle\Battle;
-use App\Service\Game\Tournament\KnightTournament\Game\SwordFighting\Config\Config;
-use App\Service\Game\Tournament\KnightTournament\Game\SwordFighting\Knight\Knight;
-use App\Service\Game\Tournament\KnightTournament\Game\SwordFighting\Weapon\Sword;
-use App\Service\Game\Tournament\KnightTournament\Participant\Participant;
-use App\Service\Game\Tournament\KnightTournament\Participant\ParticipantInterface;
-use App\Service\Game\Tournament\KnightTournament\Round\RoundInterface;
-use App\Service\Game\Tournament\KnightTournament\Round\SwordRound;
 use App\Service\Game\Tournament\KnightTournament\Tournament;
-use App\Service\Game\Tournament\Player\Player;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
